279aa9e08f4b3160cc4bc02188b23f02a36035d8
sneiergv <sneidergv@gmail.com>
2021-02-04 20:35:03 -0500

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - entry point.
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		putchar(d);
+	}
+
+	putchar('\n');
+	return (0);
+}
