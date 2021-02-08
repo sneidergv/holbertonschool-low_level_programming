e08237f53ef58b7e475a19e0c632d76efb76990b
sneiergv <sneidergv@gmail.com>
2021-02-08 12:29:53 -0500

    commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,26 +1,27 @@
 #include "holberton.h"
 
 /**
- * main - entry point
- * 
- * description: print "holberton" with putchar.
- * 
- * return: Always 0.
- */
+* main - entry point, print "holberton".
+*
+* description: print holberton with _putchar.
+*
+* Return: Always 0.
+*
+*/
 
 int main(void)
 {
-  char a;
+char a;
 
-  char i;
+char i;
 
-  char h[] = "holberton\n";
+char h[] = "holberton\n";
 
-  for (i = 0; i < 10; i++)
-  {
-    a = h[i];
+for (i = 0; i < 10; i++)
+{
+a = h[i];
 
-    _putchar(a);
-  }
-  return 0;
+_putchar(a);
+}
+return (0);
 }
