482884d3ace46a7629e4d347b43279363beb97ae
sneiergv <sneidergv@gmail.com>
2021-02-11 16:55:23 -0500

    fix9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,19 +17,23 @@ int main(void)
 	{
 		if (((count % 3) == 0) && ((count % 5) == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if ((count % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((count % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", count);
+			printf("%d", count);
+		}
+		if (count < 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
