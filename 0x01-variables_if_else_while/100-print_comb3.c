1e7846a54b750c388bb1b977bd4be481bbfd4ed9
sneiergv <sneidergv@gmail.com>
2021-02-13 21:03:33 -0500

    10-print_comb2.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible different combinations of two digits.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int first_digit, second_digit;
+
+	for (first_digit = 0; first_digit <= 9; first_digit++)
+	{
+		for (second_digit = (first_digit + 1); second_digit <= 9; second_digit++)
+		{
+			putchar(first_digit + '0');
+			putchar(second_digit + '0');
+			if (first_digit == 8 && second_digit == 9)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
