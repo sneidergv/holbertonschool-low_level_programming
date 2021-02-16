e55dade1e6f103f42d3a6aa4da33bf4e8fad0d16
sneiergv <sneidergv@gmail.com>
2021-02-16 10:05:17 -0500

    add README.md

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 * reset_to_98 -  takes a pointer to an int as parameter
 * and updates the value it points to to 98.
 *
-* @n: pointer tu reasign.
+* @n: pointer tu reassign.
 *
 * Return: Always 0.
 */
