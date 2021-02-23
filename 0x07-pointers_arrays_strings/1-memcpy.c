acaeb75580c008ee099bd2b3d5f5c55615ca5391
sneiergv <sneidergv@gmail.com>
2021-02-23 11:07:24 -0500

    fix 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 
+/**
+* _memcpy - function that copies memory area.
+* @dest: string.
+* @src: string.
+* @n: number of bytes.
+* Return: dest.
+*/
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index = 0;
