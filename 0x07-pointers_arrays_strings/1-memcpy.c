96c1e8435c05fcc03515a2beb3c147a15238fc95
sneiergv <sneidergv@gmail.com>
2021-02-23 11:12:47 -0500

    fix-3

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,12 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index = 0;
 
-	for (index = 0; index < n; index++)
+	while (index < n)
 	{
-		*(dest + index) = *(src + index);
+		*dest = *src;
+		dest++;
+		src++;
+		index++;
 	}
 	return (dest);
 }
