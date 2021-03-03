a9091dc3e255734d4dce317c430ba0b2d09dd5c2
sneiergv <sneidergv@gmail.com>
2021-03-03 18:12:53 -0500

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* str_concat - function that concatenates two strings.
+* @s1: First string to concatenate.
+* @s2: Second string to concatenate.
+* Return: Pointer to New String.
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int lenght_s1, lenght_s2;
+	char *nArray;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s1 = "";
+	}
+
+	for (lenght_s1 = 0; s1[lenght_s1]; lenght_s1++)
+		;
+	for (lenght_s2 = 0; s2[lenght_s2]; lenght_s2++)
+		;
+
+	nArray = malloc((lenght_s1 + lenght_s2 + 1) * sizeof(char));
+
+	if (nArray == NULL)
+	{
+		return (NULL);
+	}
+
+	int index_1, index_2;
+
+	for (index_1 = 0; index_1 < lenght_s1; index_1++)
+	{
+		nArray[index_1] = s1[index_1];
+	}
+
+	for (index_2 = 0; index_2 < (lenght_s1 + lenght_s2); index_2++)
+	{
+		nArray[index_1] = s2[index_2];
+		index_1++;
+	}
+	return (nArray);
+}
