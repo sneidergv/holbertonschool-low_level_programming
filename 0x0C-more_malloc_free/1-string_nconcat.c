29dd00ffa6737b5ccfa4115b3b76245e58463ba8
sneidergv <sneidergv@gmail.com>
2021-03-04 21:18:06 +0000

    fix1-

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[lenght_s2])
 		lenght_s2++;
 
-	if (n > lenght_s2)
+	if (n >= lenght_s2)
 		n = lenght_s2;
 
 	lenght_nA = (lenght_s1 + n) + 1;
