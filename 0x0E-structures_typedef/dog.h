4feb00645cccab20dd3545b54832c8a9724436ea
sneiergv <sneidergv@gmail.com>
2021-03-08 18:32:58 -0500

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - new type for structure.
+* @name: Name of a dog.
+* @age: Age of a dog.
+* @owner: Owner of a dog.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
