1b3c099c08a84c568b4928194ee68cea66923657
sneiergv <sneidergv@gmail.com>
2021-03-08 23:01:52 -0500

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - function that frees dogs.
+* @d: structure to free.
+* Return: void.
+*/
+
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+		free(d->name);
+		free(d->owner);
+		free(d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,5 +21,6 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
