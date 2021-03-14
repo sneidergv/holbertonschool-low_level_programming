5dca7bd7f76da92298da8032bf26b1867dd11730
sneiergv <sneidergv@gmail.com>
2021-03-14 16:24:24 -0500

    fix segmentation fault 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
 	int a, b, result;
 	int (*f)(int a, int b);
 
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	f = get_op_func(argv[2]);
 
