cdc09cd032ab4b69089d6055157ecba5a4111416
sneiergv <sneidergv@gmail.com>
2021-03-19 10:47:17 -0500

    0 task description added

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,17 @@
 #include "lists.h"
 
+/**
+* print_list - Write a function that prints all the
+* elements of a list_t list.
+* @h: pointer to struct first node.
+* Return: the number of nodes.
+*/
+
 size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
 	const list_t *tmp = h;
+
 	while (tmp)
 	{
 		if (tmp->str)
