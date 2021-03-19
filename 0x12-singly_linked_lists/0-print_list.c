1dcc7b415a78830120cd931bea02cb572192850f
sneiergv <sneidergv@gmail.com>
2021-03-19 10:43:00 -0500

    *tmp task_0 created

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,20 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
-	while (h)
+	const list_t *tmp = h;
+	while (tmp)
 	{
-		if (h->str)
+		if (tmp->str)
 		{
-			printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
+			printf("[%u] %s\n", tmp->len, tmp->str);
+			tmp = tmp->next;
 		}
 		else
 		{
 			printf("[0] (nil)\n");
-			h = h->next;
+			tmp = tmp->next;
 		}
 		i++;
 	}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
