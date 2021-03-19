4553fb4fa677300ca8d2d6081474f35e6df192d3
sneiergv <sneidergv@gmail.com>
2021-03-19 12:21:27 -0500

    betty check at 1-task

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,16 @@
 #include "lists.h"
 
+/**
+* list_len - Write a function that adds a new node at the
+* beginning of a list_t list.
+* @h: pointer to firs node.
+* Return: number of elements in a linked list_t list.
+*/
+
 size_t list_len(const list_t *h)
 {
 	unsigned int i = 0;
+
 	while (h)
 	{
 		i++;
