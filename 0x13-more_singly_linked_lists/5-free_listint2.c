74b375f1c57a653e68ce1082008a53e63d78a0e6
sneiergv <sneidergv@gmail.com>
2021-03-23 10:24:37 -0500

    check 5- 0x13

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,15 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *index;
+	listint_t *index = *head;
 
-	while (*head != NULL && head != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
 	{
-		index = *head;
-		*head = index->next;
+		index = index->next;
 		free(index);
+		*head = index;
 	}
 }
