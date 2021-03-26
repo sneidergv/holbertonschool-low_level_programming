62d3700878db3b97c03c11c6178f0c7cdf42a115
sneiergv <sneidergv@gmail.com>
2021-03-26 09:31:08 -0500

    1-0x14

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,16 +10,16 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int k;
+	int i;
 
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	for (int i = 31; i >= 0; i--)
+	for (i = 31; i >= 0; i--)
 	{
 		k = n >> i;
-
 		if (k & 1)
 			_putchar('1');
 		else if (k != 0)
