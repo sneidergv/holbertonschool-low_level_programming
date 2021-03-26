a0907f3c5d2c5ee4244132d38aa71f1d8f671396
sneiergv <sneidergv@gmail.com>
2021-03-25 21:34:44 -0500

    update 1-0x14

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,10 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
 #include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
-int _pow_recursion(int x, int y);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 
