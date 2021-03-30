c911b80673323a448b1ef3fa9d8b766b021fa7e9
sneidergv <sneidergv@gmail.com>
2021-03-30 19:41:48 +0000

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
-void main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	int fd1, fd2, nbytes_1, nbytes_2, cl1, cl2;
 	char buffer[1024];
 
-	if (argc != 2)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "usage: cp file_from file_to\n");
 		exit (97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
@@ -15,22 +15,24 @@ void main(int argc, char *argv)
 
 	if (fd1 < 0 || nbytes_1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't close from file %s\n", argv[1]);
 		exit (98);
 	}
-	if (argv[2] == NULL)
+	if (argv[2])
 	{
 		fd2 = open(argv[2], O_CREAT | O_WRONLY, 0664);
+		printf("archivo creado\n");
 	}
 	else
 	{
 		fd2 = open(argv[2], O_WRONLY | O_TRUNC);
+		printf("archivo creado else\n");
 	}
+	printf("num of fd2 is: %d\n", fd2);
 	nbytes_2 = write(fd2, buffer, nbytes_1);
-
 	if (fd2 < 0 || nbytes_2 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error Can't write to %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error Can't write to %s\n", argv[2]);
 		exit (99);
 	}
 	else
@@ -38,14 +40,15 @@ void main(int argc, char *argv)
 		cl1 = close(fd1);
 		if (cl1 < 0)
 		{
-			dprintf(STDERR_FILENO, "Error Can't close fd %d", fd1);
+			dprintf(STDERR_FILENO, "Error Can't close fd %d\n", fd1);
 			exit (100);
 		}
 		cl2 = close(fd2);
 		if (cl2 < 0)
 		{
-			dprintf(STDERR_FILENO, "Error Can't close fd %d", fd2);
+			dprintf(STDERR_FILENO, "Error Can't close fd %d\n", fd2);
 			exit (100);
 		}
 	}
+	return (1);
 }
