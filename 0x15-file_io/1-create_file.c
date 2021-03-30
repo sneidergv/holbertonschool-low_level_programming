d3a68f09ffc418dc99fe0e52563bf0deb0c45729
sneiergv <sneidergv@gmail.com>
2021-03-29 20:01:22 -0500

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+* create_file - function that creates a file.
+* @filename: name of the file to create.
+* @text_content: a NULL terminated string to write to the file.
+* Return: 1 on success, -1 on failure.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_file;
+	int len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	while (text_content[len])
+		len++;
+	if (len == -1)
+		return (-1);
+	write_file = write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
