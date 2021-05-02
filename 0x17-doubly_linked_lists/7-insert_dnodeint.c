701e5700c09047a6cbd78a191d085763d310c851
sneidergv <sneidergv@gmail.com>
2021-05-01 19:44:36 -0500

    _7 - Fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (i == idx - 1)
 		{
-			new_node->prev = tmp;
 			new_node->next = tmp->next;
 			tmp->next = new_node;
 			return (new_node);
