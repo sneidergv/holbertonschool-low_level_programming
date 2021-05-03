734a3253564befc9643769784b36bfbda1a8eda2
sneidergv <sneidergv@gmail.com>
2021-05-02 21:04:56 -0500

    fix_8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,7 +19,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = tmp->next;
-		tmp->next->prev = NULL;
+		(*head)->prev = NULL;
 		free(tmp);
 		return (1);
 	}
