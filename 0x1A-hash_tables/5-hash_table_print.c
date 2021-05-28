0929fc71a06c6235d902c8336463b2ea00a1f36f
sneidergv <sneidergv@gmail.com>
2021-05-28 12:53:22 -0500

    betty 5-hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: pointer to hash table.
+ * Return: void.
+ */
+
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *walker;
