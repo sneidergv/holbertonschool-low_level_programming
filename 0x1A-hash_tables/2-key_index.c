024bc2664b644bdfd4333c796f99863d8e7f6ff0
sneidergv <sneidergv@gmail.com>
2021-05-28 12:01:04 -0500

    change branch

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,15 @@
-// #include "hash_tables.h"
+#include "hash_tables.h"
 
+/**
+ * key_index - Algorithm to get the index of array.
+ * @key: key of node.
+ * @size: size of array.
+ * Return: index.
+ */
 
-// unsigned long int key_index(const unsigned char *key, unsigned long int size)
-// {
-// 	unsigned long int djb2_key = hash_djb2(key);
-// 	unsigned long int index = djb2_key % size;
-// 	return (index);
-// }
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int djb2_key = hash_djb2(key);
+	unsigned long int index = djb2_key % size;
+	return (index);
+}
