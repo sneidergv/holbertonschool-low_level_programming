1c06eccb1fa5bcf2f073ef88ad49b83b54007b9d
sneidergv <sneidergv@gmail.com>
2021-05-28 14:44:53 -0500

    freless 3-hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
-			free(tmp->value);
 			tmp->value = _value;
 			return (1);
 		}
