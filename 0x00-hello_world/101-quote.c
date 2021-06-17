f5c2920e641c02de63a9de082b0ccdecafaaf990
Esneider Granada Valencia <esneiderimac@iMac-de-Esneider.local>
2021-06-17 15:50:10 -0500

    fix 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <unistd.h>
 
+/**
+ * main - print a string in stderr
+ * Return: 1
+ */
 int main()
 {
     write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 87);
